b191a5a024cd7f50fc164f293f9857159ab5a7a5
Luca Atella <atellaluca@outlook.it>
2024-10-04 21:08:33 +0200

    docs: update toctree and remove 'for' from index.html
    - Added `importspy` and `example` to the toctree in `index.rst` to include the new example section in the documentation.
    - Removed the incorrect use of 'for' from `index.html` for better clarity and structure.
    - Followed reStructuredText syntax for proper documentation formatting.

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -6,12 +6,9 @@
 ImportSpy documentation
 =======================
 
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
-
-
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
+   importspy
+   example
--- example/package.py
+++ example/package.py
@@ -1,10 +1,9 @@
 from importspy import Spy
 from types import ModuleType
 import inspect
-
 from plugin_interface import Plugin
 
-def condition(module:ModuleType):
+def condition(module: ModuleType) -> bool:
     for class_name, class_obj in inspect.getmembers(module, inspect.isclass):
         if issubclass(class_obj, Plugin) and class_obj is not Plugin:
             return True
