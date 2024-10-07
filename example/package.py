b255a53f3d710ad540a7002ac4faaf23e1f64284
Luca Atella <atellaluca@outlook.it>
2024-10-07 21:48:54 +0200

    refactor: remove condition function and streamline PluginSpy implementation

--- example/package.py
+++ example/package.py
@@ -1,18 +1,8 @@
 from importspy import Spy
 from importspy.models import SpyModel, ClassModel
-from types import ModuleType
-import inspect
-from plugin_interface import Plugin
 from typing import List
 
 class PluginSpy(SpyModel):
     classes: List[ClassModel] = [ClassModel(superclasses=["Plugin"])]
 
-def condition(module: ModuleType) -> bool:
-    for class_name, class_obj in inspect.getmembers(module, inspect.isclass):
-        if issubclass(class_obj, Plugin) and class_obj is not Plugin:
-            return True
-    return False
-
-print(Spy().importspy(validation=condition))
 print(Spy().importspy(spymodel=PluginSpy))
