70a525b99a74aabc326da37865767ad5f2b2f4aa
Luca Atella <atellaluca@outlook.it>
2024-10-07 21:21:16 +0200

    feat: Extend is_subset function to include filename and version comparisons
    - Modified the `is_subset` function to also compare the `filename` and `version` attributes
      of the `SpyModel` instances in addition to the existing checks for functions and classes.
    - Ensured that only non-empty values are considered for the subset comparison.

--- src/importspy/utils/model_utils.py
+++ src/importspy/utils/model_utils.py
@@ -1,6 +1,32 @@
 from importspy.models import SpyModel
 
 def is_subset(spy_model_1: SpyModel, spy_model_2: SpyModel) -> bool:
+    """
+    Check if the first SpyModel is a subset of the second SpyModel.
+
+    This function compares two `SpyModel` instances to determine if the first model
+    is a subset of the second model. It verifies that all non-empty functions and classes 
+    defined in the first model are present in the second model, along with their respective 
+    methods, superclasses, filename, and version. If any function, class, or other attribute
+    in the first model is empty, it is ignored in the subset check.
+
+    :param spy_model_1: 
+        The first `SpyModel` instance to check.
+    :type spy_model_1: SpyModel
+
+    :param spy_model_2: 
+        The second `SpyModel` instance against which the first model is checked.
+    :type spy_model_2: SpyModel
+
+    :return: 
+        True if all non-empty functions, classes, and other attributes in `spy_model_1` 
+        are also present in `spy_model_2`, False otherwise.
+    :rtype: bool
+    """
+    if spy_model_1.filename and spy_model_1.filename != spy_model_2.filename:
+        return False
+    if spy_model_1.version and spy_model_1.version != spy_model_2.version:
+        return False
     if spy_model_1.functions:
         if not set(spy_model_1.functions).issubset(spy_model_2.functions):
             return False
@@ -15,5 +41,6 @@ def is_subset(spy_model_1: SpyModel, spy_model_2: SpyModel) -> bool:
             if not set(class_1.superclasses).issubset(class_2.superclasses):
                 return False
     return True
+
 
     
