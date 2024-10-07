002087e31aec0fe0f3c6a96c14dd745c438f580d
Luca Atella <atellaluca@outlook.it>
2024-10-07 18:48:54 +0200

    refactor: enhance SpyModel with module extraction and validators

--- src/importspy/models.py
+++ src/importspy/models.py
@@ -1,36 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 from typing import Optional, List
 from types import ModuleType
+from .utils import module_utils
+import logging
 
-
-class FunctionModel(BaseModel):
-    """
-    Model representing a function within a module.
-
-    :param name: The name of the function. It can be `None` if the name is not available.
-    :type name: Optional[str]
-
-    :example:
-        function = FunctionModel(name="my_function")
-    """
-    name: Optional[str] = None
-
+logger = logging.getLogger("/".join(__file__.split('/')[-2:]))
+logger.addHandler(logging.NullHandler())
 
 class ClassModel(BaseModel):
     """
     Model representing a class within a module.
 
-    :param name: The name of the class. It can be `None` if the name is not available.
-    :type name: Optional[str]
+    Attributes:
+        name (Optional[str]): The name of the class. It can be `None` if the name is not available. Defaults to `None`.
+        methods (Optional[List[str]]): A list of method names defined in the class. Defaults to an empty list.
+        superclasses (Optional[List[str]]): A list of names of the superclasses from which this class inherits. Defaults to an empty list.
 
-    :param methods: A list of method names defined in the class. Defaults to an empty list.
-    :type methods: Optional[List[str]]
-
-    :param superclasses: A list of names of the superclasses from which this class inherits. Defaults to an empty list.
-    :type superclasses: Optional[List[str]]
-
-    :example:
-        class_model = ClassModel(name="MyClass", methods=["method1", "method2"], superclasses=["BaseClass"])
+    Example:
+        class_model = ClassModel(
+            name="MyClass", 
+            methods=["method1", "method2"], 
+            superclasses=["BaseClass"]
+        )
     """
     name: Optional[str] = None
     methods: Optional[List[str]] = []
@@ -41,36 +32,50 @@ class SpyModel(BaseModel):
     """
     Model representing metadata of a module, including information about its functions and classes.
 
-    :param name: The name of the module. It can be `None` if the name is not available.
-    :type name: Optional[str]
+    Attributes:
+        name (Optional[str]): The name of the module. It can be `None` if the name is not available. Defaults to `None`.
+        version (Optional[str]): The version of the module. It can be `None` if the version is not available. Defaults to `None`.
+        functions (Optional[List[str]]): A list of function names defined in the module. Defaults to an empty list.
+        classes (Optional[List[ClassModel]]): A list of classes defined in the module, represented by `ClassModel`. Defaults to an empty list.
+        superclasses (Optional[List[str]]): A list of superclasses from which module-level classes inherit. Defaults to an empty list.
 
-    :param version: The version of the module. It can be `None` if the version is not available.
-    :type version: Optional[str]
-
-    :param author: The name of the author of the module. It can be `None` if the author information is not available.
-    :type author: Optional[str]
-
-    :param functions: A list of functions defined in the module, represented by the `FunctionModel`. Defaults to an empty list.
-    :type functions: Optional[List[FunctionModel]]
-
-    :param classes: A list of classes defined in the module, represented by the `ClassModel`. Defaults to an empty list.
-    :type classes: Optional[List[ClassModel]]
-
-    :param superclasses: A list of superclasses from which module-level classes inherit. Defaults to an empty list.
-    :type superclasses: Optional[List[str]]
-
-    :example:
+    Example:
         spy_model = SpyModel(
             name="MyModule",
             version="1.0.0",
-            author="John Doe",
-            functions=[FunctionModel(name="my_function")],
-            classes=[ClassModel(name="MyClass", methods=["my_method"], superclasses=["BaseClass"])]
+            functions=["my_function"],
+            classes=[
+                ClassModel(name="MyClass", methods=["my_method"], superclasses=["BaseClass"])
+            ]
         )
+
+    Validator:
+        deserialize_info_module:
+            Pre-validator method that extracts information from a `ModuleType` object and populates the fields of the `SpyModel`.
+            This includes extracting the module name, version, functions, and classes.
+    
+    Args:
+        info_module (ModuleType): The module from which to extract metadata.
     """
-    name: Optional[str] = None
+
+    filename: Optional[str] = None
     version: Optional[str] = None
-    author: Optional[str] = None
-    functions: Optional[List[FunctionModel]] = []
+    functions: Optional[List[str]] = []
     classes: Optional[List[ClassModel]] = []
-    superclasses: Optional[List[str]] = []
+
+    @classmethod
+    def from_module(cls, info_module: ModuleType):
+        logger.debug(f"Create SpyModel from info_module: {ModuleType}")
+        filename = "/".join(info_module.__file__.split('/')[-1:])
+        version = module_utils.extract_version(info_module)
+        functions = module_utils.extract_functions(info_module)
+        classes = [
+            ClassModel(name=name, methods=methods, superclasses=superclasses)
+            for name, methods, superclasses in module_utils.extract_classes(info_module)
+        ]
+        return cls(
+            filename=filename,
+            version=version,
+            functions=functions,
+            classes=classes
+        )
