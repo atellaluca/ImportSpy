faac43a75fe5231b567ece522939f4be746d3a58
Luca Atella <atellaluca@outlook.it>
2024-10-08 12:44:30 +0200

    feat: enhance Extension class with extension management methods
    - Added `add_extension` and `remove_extension` methods to the `Extension` class to manage extension lifecycle events, with corresponding print statements for feedback.
    - Introduced `http_get_request` method in the `Extension` class to simulate an HTTP GET request, providing a placeholder for future implementation.
    - Created a new `Foo` class with a `get_bar` method that outputs a "Foobar" message, adding additional functionality to the module.
    - These enhancements aim to improve the usability of the `Extension` class and provide a basic structure for further development.

--- example/extension.py
+++ example/extension.py
@@ -1,4 +1,18 @@
-from .plugin_interface import Plugin
+import package
+from plugin_interface import Plugin
 
 class Extension(Plugin):
-    pass
+    
+    def add_extension(self):
+        print("Extension has added")
+    
+    def remove_extension(self):
+        print("Extension has removed")
+    
+    def http_get_request(self):
+        print("done")
+
+class Foo:
+
+    def get_bar(self):
+        print("Foobar")
