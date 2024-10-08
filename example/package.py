bb1d3ae0ecc6cc5c13e07bb88d9f3d612ddda1b5
Luca Atella <atellaluca@outlook.it>
2024-10-08 12:45:19 +0200

    feat: update PluginSpy class to include method definitions and filename

--- example/package.py
+++ example/package.py
@@ -3,6 +3,15 @@ from importspy.models import SpyModel, ClassModel
 from typing import List
 
 class PluginSpy(SpyModel):
-    classes: List[ClassModel] = [ClassModel(superclasses=["Plugin"])]
+    classes: List[ClassModel] = [
+        ClassModel(
+            methods=["add_extension", "remove_extension", "http_get_request"],
+            superclasses=["Plugin"]
+        ),
+        ClassModel(
+            name="Foo",
+            methods=["get_bar"]
+        )]
+    filename: str = "extension.py"
 
 print(Spy().importspy(spymodel=PluginSpy))
