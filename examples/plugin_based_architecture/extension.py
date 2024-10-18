22a66c4b243c2b1f6677a2c6a7bc7cc3d5a8bdd4
Luca Atella <atellaluca@outlook.it>
2024-10-18 22:29:28 +0200

    feat(examples): add class and instance attributes

--- examples/plugin_based_architecture/extension.py
+++ examples/plugin_based_architecture/extension.py
@@ -1,7 +1,14 @@
 import package
 from plugin_interface import Plugin
 
+plugin_name = "plugin name"
+plugin_description = "plugin description"
 class Extension(Plugin):
+
+    extension_name = "extension_name"
+
+    def __init__(self) -> None:
+        self.extension_instance_name = "extension_instance_name"
     
     def add_extension(self):
         print("Extension has added")
--- examples/plugin_based_architecture/package.py
+++ examples/plugin_based_architecture/package.py
@@ -4,8 +4,11 @@ from importspy.utils import spy_model_utils
 from typing import List
 
 class PluginSpy(SpyModel):
+    variables: List[str] = ["plugin_name", "plugin_description"]
     classes: List[ClassModel] = [
         ClassModel(
+            class_attr=["extension_name"],
+            instance_attr=["extension_instance_name"],
             methods=["add_extension", "remove_extension", "http_get_request"],
             superclasses=["Plugin"]
         ),
