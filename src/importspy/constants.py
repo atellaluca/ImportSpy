fedd0c2df58d1f6886d9cfbfa14298161b50998e
Luca Atella <atellaluca@outlook.it>
2024-12-29 19:37:52 +0100

    docs(constants): add detailed docstrings for Constants class

--- src/importspy/constants.py
+++ src/importspy/constants.py
@@ -1,12 +1,30 @@
 class Constants:
+    """
+    A central repository for static definitions used throughout the ImportSpy package.
 
+    This class defines known architecture constants and provides a standardized reference
+    for supported system architectures. These constants are utilized to identify and handle
+    platform-specific module requirements in ImportSpy.
+
+    Attributes:
+        KNOWN_ARCHITECTURES (list[str]): A list of all supported system architectures.
+        ARCH_x86_64 (str): Identifier for the x86_64 architecture.
+        ARCH_AARCH64 (str): Identifier for the aarch64 architecture.
+        ARCH_ARM (str): Identifier for the ARM architecture.
+        ARCH_ARM64 (str): Identifier for the ARM64 architecture.
+        ARCH_I386 (str): Identifier for the i386 architecture.
+        ARCH_PPC64 (str): Identifier for the PPC64 architecture.
+        ARCH_PPC64LE (str): Identifier for the PPC64LE architecture.
+        ARCH_S390X (str): Identifier for the S390X architecture.
+    """
+    
     KNOWN_ARCHITECTURES = ['x86_64', 'aarch64', 'arm', 'arm64', 'i386', 'ppc64', 'ppc64le', 's390x']
 
     ARCH_x86_64 = "x86_64"
-    ARCH_AARCH64 ='aarch64'
-    ARCH_ARM ='arm'
-    ARCH_ARM64 ='arm64'
-    ARCH_I386 ='i386'
-    ARCH_PPC64 ='ppc64'
-    ARCH_PPC64LE ='ppc64le'
-    ARCH_S390X ='s390x'
+    ARCH_AARCH64 = "aarch64"
+    ARCH_ARM = "arm"
+    ARCH_ARM64 = "arm64"
+    ARCH_I386 = "i386"
+    ARCH_PPC64 = "ppc64"
+    ARCH_PPC64LE = "ppc64le"
+    ARCH_S390X = "s390x"
