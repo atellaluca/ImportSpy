3b89197ba486dd01263debdc6bacd1b4d0398721
Luca Atella <atellaluca@outlook.it>
2024-12-29 19:06:12 +0100

    refactor: remove deprecated validation logic from Spy.importspy
    - Removed the `validation` parameter from the `importspy` method:
      - Deprecated functionality and warnings related to `validation` have been eliminated.
      - Simplified the method to rely solely on `SpyModel` for validation.
    
    - Updated the docstring of `importspy` to reflect the removal of the `validation` parameter:
      - Removed the mention of deprecation warnings and the legacy validation behavior.
    
    - Improved readability by eliminating unnecessary imports, including `warnings` and `Callable`.
    
    This refactor streamlines the `Spy` class by enforcing the use of `SpyModel` for module validation, improving maintainability and aligning with the latest design decisions.

--- src/importspy/s.py
+++ src/importspy/s.py
@@ -1,10 +1,8 @@
-from typing import Callable
 from types import ModuleType
 from .errors import Errors
 from .models import SpyModel
 from .utils import spy_model_utils, spy_module_utils
 import logging
-import warnings
 
 logger = logging.getLogger("/".join(__file__.split('/')[-2:]))
 logger.addHandler(logging.NullHandler())
@@ -23,7 +21,7 @@ class Spy:
         Use caution when dynamically importing code, particularly from untrusted sources, to avoid executing malicious code.
     """
 
-    def importspy(self, spymodel: SpyModel | None = None, validation: Callable[[ModuleType], bool] | None = None) -> ModuleType:
+    def importspy(self, spymodel: SpyModel | None = None) -> ModuleType:
         """
         Dynamically imports the module that called this function, with optional validation against a `SpyModel` 
         or a custom validation function.
@@ -36,26 +34,12 @@ class Spy:
         spymodel : SpyModel, optional
             An instance of `SpyModel` that defines the rules the imported module should follow. If provided, 
             the module is only loaded if it adheres to the model.
-        validation : Callable[[ModuleType], bool], optional
-            A deprecated validation function that returns `True` if the module passes validation. 
-            It will be removed in future versions.
 
         Returns:
         --------
         ModuleType
             The imported module if it passes the validation, or `None` if it does not comply or no validation 
             is applied.
-
-        Raises:
-        -------
-        DeprecationWarning
-            If the `validation` parameter is used, a warning is raised as this parameter will be removed in 
-            future versions.
-
-        .. warning::
-
-            The `validation` parameter is deprecated as of version 0.1.6 due to security concerns. Use the 
-            `SpyModel` for validation instead.
 
         Example:
         --------
@@ -71,14 +55,6 @@ class Spy:
             spy_module = SpyModel.from_module(info_module)
             logger.debug(f"Spy module: {spy_module}")
             return spy_module_utils.load_module(info_module) if spy_model_utils.is_subset(spymodel(), spy_module) else None
-        elif validation:
-            warnings.warn(
-                "The `validation` parameter is deprecated as of version 0.1.6 due to security vulnerabilities. "
-                "It will be removed in future versions. Please use the `importspy` method with `SpyModel` instead.",
-                DeprecationWarning
-            )
-            module = spy_module_utils.load_module(info_module)
-            return module if validation(module) else None
         return spy_module_utils.load_module(info_module)
 
     def _spy_module(self) -> ModuleType | None:
