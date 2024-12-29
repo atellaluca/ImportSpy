975e3e84fcae61031355d0cee0fa5abd82e362a0
Luca Atella <atellaluca@outlook.it>
2024-12-29 18:55:17 +0100

    feat: centralize supported architectures in a constants file

---
+++ src/importspy/constants.py
@@ -0,0 +1,12 @@
+class Constants:
+
+    KNOWN_ARCHITECTURES = ['x86_64', 'aarch64', 'arm', 'arm64', 'i386', 'ppc64', 'ppc64le', 's390x']
+
+    ARCH_x86_64 = "x86_64"
+    ARCH_AARCH64 ='aarch64'
+    ARCH_ARM ='arm'
+    ARCH_ARM64 ='arm64'
+    ARCH_I386 ='i386'
+    ARCH_PPC64 ='ppc64'
+    ARCH_PPC64LE ='ppc64le'
+    ARCH_S390X ='s390x'
