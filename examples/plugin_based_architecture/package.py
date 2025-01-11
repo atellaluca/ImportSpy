5b347409d3263e2c8e2a5a050290409b64fa9011
Luca Atella <atellaluca@outlook.it>
2025-01-11 22:36:51 +0100

    🔄 Refactor: Enhanced plugin configuration with new runtime and attribute models
    - Added `__version__` to indicate the version of the plugin configuration.
    - Introduced a second runtime targeting ARM64 and macOS.
    - Replaced `class_attr` and `instance_attr` with structured `attributes` using the `Attribute` model.
    - Enhanced method list for the `Extension` class, including `__init__`.

--- examples/plugin_based_architecture/package.py
+++ examples/plugin_based_architecture/package.py
@@ -9,10 +9,13 @@ from importspy.models import (
     System,
     Python,
     Module,
-    Class
+    Class,
+    Attribute
     )
 
 from typing import List
+
+__version__ = None
 
 class PluginSpy(SpyModel):
     deployments: List[Deployment] = [
@@ -25,6 +28,14 @@ class PluginSpy(SpyModel):
                             os=Constants.OS_LINUX
                         )
                     ]
+                ),
+                Runtime(
+                    arch=Constants.ARCH_ARM64,
+                    systems=[
+                        System(
+                            os=Constants.OS_MACOS
+                        )
+                    ]
                 )
             ],
         )
@@ -33,14 +44,24 @@ class PluginSpy(SpyModel):
     classes: List[Class] = [
         Class(
             name="Extension",
-            class_attr=["extension_name"],
-            instance_attr=["extension_instance_name"],
-            methods=["add_extension", "remove_extension", "http_get_request"],
+            attributes=[
+                Attribute(
+                    type=Constants.INSTANCE_TYPE,
+                    name="extension_instance_name",
+                    value="extension_instance_value"
+                ),
+                Attribute(
+                    type=Constants.CLASS_TYPE,
+                    name="extension_name",
+                    value="extension_value"
+                )
+            ],
+            methods=["__init__", "add_extension", "remove_extension", "http_get_request"],
             superclasses=["Plugin"]
         ),
         Class(
             name="Foo",
-            methods=["get_bar"]
+            methods=["get_bar"],
         )]
     filename: str = "extension.py"
 
