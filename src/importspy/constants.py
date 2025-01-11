abec4cb7f4228d9804a379f4fc266920fdb0f7d2
Luca Atella <atellaluca@outlook.it>
2025-01-11 23:59:24 +0100

    ♻️ Refactor: Refactored constants.py to delegate values to Config
    - Removed inline constant values in `Constants` and replaced them with references to `Config`.
    - Delegated architecture, OS, Python implementations, and attribute types to the `Config` class.
    - Centralized all static values in `Config` for easier maintenance and consistency.

--- src/importspy/constants.py
+++ src/importspy/constants.py
@@ -1,50 +1,39 @@
+from .config import Config
+
 class Constants:
-
-    """
-    This class defines known architecture constants and provides a standardized reference
-    for supported system architectures. These constants are utilized to identify and handle
-    platform-specific module requirements in ImportSpy.
-
-    Attributes:
-        KNOWN_ARCHITECTURES (list[str]): A list of all supported system architectures.
-        ARCH_x86_64 (str): Identifier for the x86_64 architecture.
-        ARCH_AARCH64 (str): Identifier for the aarch64 architecture.
-        ARCH_ARM (str): Identifier for the ARM architecture.
-        ARCH_ARM64 (str): Identifier for the ARM64 architecture.
-        ARCH_I386 (str): Identifier for the i386 architecture.
-        ARCH_PPC64 (str): Identifier for the PPC64 architecture.
-        ARCH_PPC64LE (str): Identifier for the PPC64LE architecture.
-        ARCH_S390X (str): Identifier for the S390X architecture.
-    """
     
-    KNOWN_ARCHITECTURES = ['x86_64', 'aarch64', 'arm', 'arm64', 'i386', 'ppc64', 'ppc64le', 's390x']
+    KNOWN_ARCHITECTURES = [Config.ARCH_x86_64, 
+                           Config.ARCH_AARCH64, 
+                           Config.ARCH_ARM, 
+                           Config.ARCH_ARM64, 
+                           Config.ARCH_I386, 
+                           Config.ARCH_PPC64, 
+                           Config.ARCH_PPC64LE, 
+                           Config.ARCH_S390X]
+
+    SUPPORTED_OS = [Config.OS_WINDOWS,
+                    Config.OS_LINUX, 
+                    Config.OS_MACOS]
+
+    SUPPORTED_PYTHON_IMPLEMENTATION = [Config.INTERPRETER_CPYTHON,
+                                       Config.INTERPRETER_PYPY,
+                                       Config.INTERPRETER_JYTHON,
+                                       Config.INTERPRETER_IRON_PYTHON,
+                                       Config.INTERPRETER_MICROPYTHON,
+                                       Config.INTERPRETER_BRYTHON,
+                                       Config.INTERPRETER_PYSTON,
+                                       Config.INTERPRETER_GRAALPYTHON,
+                                       Config.INTERPRETER_RUSTPYTHON,
+                                       Config.INTERPRETER_NUITKA,
+                                       Config.INTERPRETER_TRANSCRYPT]
     
-    ARCH_x86_64 = "x86_64"
-    ARCH_AARCH64 = "aarch64"
-    ARCH_ARM = "arm"
-    ARCH_ARM64 = "arm64"
-    ARCH_I386 = "i386"
-    ARCH_PPC64 = "ppc64"
-    ARCH_PPC64LE = "ppc64le"
-    ARCH_S390X = "s390x"
-
-    SUPPORTED_OS = ["windows", "linux", "darwin"]
-    OS_WINDOWS = "windows"
-    OS_LINUX = "linux"
-    OS_MACOS = "darwin"
-
-    SUPPORTED_PYTHON_IMPLEMENTATION = ["CPython", "PyPy", "Jython", "IronPython"]
-    INTERPRETER_CPYTHON = "CPython"
-    INTERPRETER_PYPY = "PyPy"
-    INTERPRETER_JYTHON = "Jython"
-    INTERPRETER_IRON_PYTHON = "IronPython"
-
-    ATTRIBUTE_TYPES = ["class", "instance"]
-
-    CLASS_TYPE = "class"
-    INSTANCE_TYPE = "instance"
+    ATTRIBUTE_TYPES = [Config.CLASS_TYPE,
+                       Config.INSTANCE_TYPE]
 
     DIE_ENVIRONMENT = "DIE - Default ImportSpy Environment"
 
     NAME = "name"
     VALUE = "value"
+
+    CLASS_TYPE = Config.CLASS_TYPE
+    INSTANCE_TYPE = Config.INSTANCE_TYPE
