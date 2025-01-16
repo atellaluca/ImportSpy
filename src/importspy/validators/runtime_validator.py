94b40148983d26e069c5664b6ab16e4c0bd6c570
Luca Atella <atellaluca@outlook.it>
2025-01-16 11:28:01 +0100

    fix: add handling for missing runtime configurations in validation
    - Enhanced validation to return `True` when the first runtime exists and the second is missing.
    - Returns `False` for cases where the second runtime configuration is not provided.

--- src/importspy/validators/runtime_validator.py
+++ src/importspy/validators/runtime_validator.py
@@ -3,6 +3,12 @@ from ..models import Runtime
 class RuntimeValidator:
     
     def validate(self, runtime_1: Runtime, runtime_2: Runtime):
+        if not runtime_1:
+            return
+        if runtime_1 and not runtime_2:
+            return True
+        if not runtime_2:
+            return False
         if runtime_1.arch == runtime_2.arch:
             return True
         return False
