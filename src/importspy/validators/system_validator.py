753a40255c232a7458422d4305a526b246c9aef1
Luca Atella <atellaluca@outlook.it>
2025-01-16 11:28:19 +0100

    fix: improve validation logic for system-level configurations
    - Added checks to handle cases where the first or second system configuration is missing.
    - Ensured environment variable validation is performed only when defined.

--- src/importspy/validators/system_validator.py
+++ src/importspy/validators/system_validator.py
@@ -7,9 +7,17 @@ class SystemValidator:
     def validate(self,
                  system_1:System,
                  system_2:System):
+        if not system_1:
+            return None
+        if system_1 and not system_2:
+            return True
+        if not system_2:
+            return False
         cv = CommonValidator()
         if system_1.os == system_2.os:
-            return cv.dict_validate(system_1.envs, system_2.envs, Errors.ENV_VAR_MISSING, Errors.ENV_VAR_MISMATCH)
+            if system_1.envs:
+                return cv.dict_validate(system_1.envs, system_2.envs, Errors.ENV_VAR_MISSING, Errors.ENV_VAR_MISMATCH)
+            return True
         return False
         
         
