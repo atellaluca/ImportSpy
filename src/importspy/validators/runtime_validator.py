ad6837c4faf3f1370b5f65be4e6eccf05b3d902d
Luca Atella <atellaluca@outlook.it>
2025-01-17 20:51:39 +0100

    refactor(runtime-validator): add system validation and refactor runtime validation logic

--- src/importspy/validators/runtime_validator.py
+++ src/importspy/validators/runtime_validator.py
@@ -1,14 +1,26 @@
-from ..models import Runtime
+from ..models import (
+    Runtime,
+    System
+)
+from typing import List
+from ..errors import Errors
+from .system_validator import SystemValidator
 
 class RuntimeValidator:
+
+    def __init__(self):
+        self._system_validator = SystemValidator()
     
     def validate(self, runtime_1: Runtime, runtime_2: Runtime):
         if not runtime_1:
             return
-        if runtime_1 and not runtime_2:
-            return True
         if not runtime_2:
-            return False
+            raise(ValueError(Errors.ELEMENT_MISSING.format(runtime_1)))
         if runtime_1.arch == runtime_2.arch:
-            return True
+            self._systems_validate(runtime_1.systems, runtime_2.systems[0])
+            return
         return False
+    
+    def _systems_validate(self, systems_1:List[System], system_2:System):
+        for system_1 in systems_1:
+            self._system_validator.validate(system_1, system_2)
