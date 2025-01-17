b1e889614580b1e7badf3023a318323e44c5b028
Luca Atella <atellaluca@outlook.it>
2025-01-17 20:50:12 +0100

    feat(common-validator): add enhanced list and dict validation with logging support

--- src/importspy/validators/common_validator.py
+++ src/importspy/validators/common_validator.py
@@ -1,12 +1,30 @@
+from ..errors import Errors
+from ..constants import Constants
+from ..log_manager import LogManager
+
 class CommonValidator:
+
+    def __init__(self):
+        self.logger = LogManager().get_logger(self.__class__.__name__)
 
     def list_validate(self,
                       list1:list, 
                       list2:list, 
                       missing_error:str, 
                       *args):
+        self.logger.debug(
+            Constants.LOG_MESSAGE_TEMPLATE.format(
+                operation="List validating",
+                status="Starting",
+                details=f"Expected list: {list1} ; Current list: {list2}"
+            )
+        )
         if not list1:
             return
+        if list1 and not list2:
+            return
+        if not list2:
+            raise ValueError(Errors.ELEMENT_MISSING.format(list1))
         for expected_element in list1:
             if expected_element not in list2:
                 raise ValueError(missing_error.format(expected_element, *args))
@@ -16,6 +34,13 @@ class CommonValidator:
                       dict2:dict,
                       missing_error:str,
                       mismatch_error:str):
+        self.logger.debug(
+            Constants.LOG_MESSAGE_TEMPLATE.format(
+                operation="Dict validating",
+                status="Starting",
+                details=f"Expected dict: {dict1} ; Current dict: {dict2}"
+            )
+        )
         if not dict1:
             return
         if not dict2:
