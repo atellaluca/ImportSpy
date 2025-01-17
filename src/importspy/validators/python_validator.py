3b2ff8a8ee543f0b78fd7da09b47a51552205276
Luca Atella <atellaluca@outlook.it>
2025-01-17 20:51:25 +0100

    refactor(python-validator): update to utilize ModuleValidator for module validation

--- src/importspy/validators/python_validator.py
+++ src/importspy/validators/python_validator.py
@@ -1,9 +1,11 @@
 from ..models import Python
 from typing import List
 from .module_validator import ModuleValidator
-from ..errors import Errors
 
 class PythonValidator:
+
+    def __init__(self):
+        self._module_validator = ModuleValidator()
 
     def validate(self,
                  python_1:Python,
@@ -14,6 +16,7 @@ class PythonValidator:
             return True
         if not python_2:
             return False
+        self._module_validator.validate(python_1.modules, python_2.modules[0])
         if python_1.version == python_2.version \
             and python_1.interpreter == python_2.interpreter:
             return True
