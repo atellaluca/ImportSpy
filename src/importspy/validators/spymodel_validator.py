21e5f365a260648fc121e72cf0191d4064b99d64
Luca Atella <atellaluca@outlook.it>
2025-01-17 20:51:56 +0100

    refactor(spymodel-validator): simplify logic and enhance modular validation integration

--- src/importspy/validators/spymodel_validator.py
+++ src/importspy/validators/spymodel_validator.py
@@ -1,28 +1,22 @@
 from ..models import (
     SpyModel,
     Runtime,
-    System,
-    Python,
-    Module
 )
 from .runtime_validator import RuntimeValidator
-from .system_validator import SystemValidator
 from .module_validator import ModuleValidator
-from .python_validator import PythonValidator
 from typing import List
 
 class SpyModelValidator:
 
-    runtime_validator = RuntimeValidator()
-    system_validator = SystemValidator()
-    python_validator = PythonValidator()
-    module_validator = ModuleValidator()
+    def __init__(self):
+        self._runtime_validator = RuntimeValidator()
+        self._module_validator = ModuleValidator()
 
     def validate(self,
                   spy_model_1: SpyModel,
-                  spy_model_2: SpyModel) -> bool:
+                  spy_model_2: SpyModel):
         self._runtimes_validate(spy_model_1.deployments, spy_model_2.deployments[0])        
-        self.module_validator.validate(spy_model_1, 
+        self._module_validator.validate([spy_model_1], 
                                        spy_model_2
                                         .deployments[0]
                                         .systems[0]
@@ -32,24 +26,7 @@ class SpyModelValidator:
     def _runtimes_validate(self, runtimes_1:List[Runtime], runtime_2:Runtime):
         if not runtimes_1:
             return
-        system_2 = runtime_2.systems[0]
         for runtime_1 in runtimes_1:
-            self.runtime_validator.validate(runtime_1, runtime_2)
-            self._systems_validate(runtimes_1, system_2)
+            self._runtime_validator.validate(runtime_1, runtime_2)
             return
-
-    def _systems_validate(self, systems_1:List[System], system_2:System):
-        python_2 = system_1.pythons[0]
-        for system_1 in systems_1:
-            self.system_validator.validate(system_1, system_2)
-            self._pythons_validate(system_1.pythons, python_2)
-    
-    def _pythons_validate(self, pythons_1:List[Python], python_2:Python):
-        for python_1 in pythons_1:
-            self.python_validator.validate(python_1, python_2)
-            
-    def _modules_validate(self, modules_1: List[Module], modules_2: List[Module]):
-        for module_1 in modules_1:
-            module_2 = next((module for module in modules_2 if module_1.filename == module_1.filename), None)
-            self.module_validator.validate(module_1, module_2)
         
