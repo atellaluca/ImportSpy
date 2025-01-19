6ba559984df7c0d5645deb74b6ec04b962c588ac
Luca Atella <atellaluca@outlook.it>
2025-01-19 13:03:00 +0100

    feat: Enhance PythonValidator with logging and improved error handling for missing elements

--- src/importspy/validators/python_validator.py
+++ src/importspy/validators/python_validator.py
@@ -1,23 +1,32 @@
 from ..models import Python
-from typing import List
+from ..errors import Errors
 from .module_validator import ModuleValidator
+from ..log_manager import LogManager
+from ..constants import Constants
 
 class PythonValidator:
 
     def __init__(self):
+        self.logger = LogManager().get_logger(self.__class__.__name__)
         self._module_validator = ModuleValidator()
 
     def validate(self,
                  python_1:Python,
                  python_2:Python):
+        self.logger.debug(
+            Constants.LOG_MESSAGE_TEMPLATE.format(
+                operation="Python validating",
+                status="Starting",
+                details=f"Expected python: {python_1} ; Current python: {python_2}"
+            )
+        )
         if not python_1:
             return
-        if python_1 and not python_2:
-            return True
         if not python_2:
-            return False
-        self._module_validator.validate(python_1.modules, python_2.modules[0])
+            raise(ValueError(Errors.ELEMENT_MISSING.format(python_1)))
         if python_1.version == python_2.version \
             and python_1.interpreter == python_2.interpreter:
+            if python_2.modules:
+                self._module_validator.validate(python_1.modules, python_2.modules[0])
             return True
         return False
