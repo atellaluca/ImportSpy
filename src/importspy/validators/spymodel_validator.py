824b65c6b7c84bf9d6233b1b8b409b431bbd66a1
Luca Atella <atellaluca@outlook.it>
2025-01-19 15:23:54 +0100

    refactor: Simplify SpyModelValidator by removing redundant runtime validation methods

--- src/importspy/validators/spymodel_validator.py
+++ src/importspy/validators/spymodel_validator.py
@@ -4,7 +4,6 @@ from ..models import (
 )
 from .runtime_validator import RuntimeValidator
 from .module_validator import ModuleValidator
-from typing import List
 
 class SpyModelValidator:
 
@@ -15,18 +14,11 @@ class SpyModelValidator:
     def validate(self,
                   spy_model_1: SpyModel,
                   spy_model_2: SpyModel):
-        self._runtimes_validate(spy_model_1.deployments, spy_model_2.deployments[0])        
+        self._runtime_validator.validate(spy_model_1.deployments, spy_model_2.deployments)        
         self._module_validator.validate([spy_model_1], 
                                        spy_model_2
                                         .deployments[0]
                                         .systems[0]
                                         .pythons[0]
                                         .modules[0])
-    
-    def _runtimes_validate(self, runtimes_1:List[Runtime], runtime_2:Runtime):
-        if not runtimes_1:
-            return
-        for runtime_1 in runtimes_1:
-            self._runtime_validator.validate(runtime_1, runtime_2)
-            return
         
