2e6f21ce3079f8ac5f53604a721b5635cb23aa90
Luca Atella <atellaluca@outlook.it>
2025-01-19 15:23:37 +0100

    feat: Update RuntimeValidator to support list-based validation and streamline system validation

--- src/importspy/validators/runtime_validator.py
+++ src/importspy/validators/runtime_validator.py
@@ -11,17 +11,14 @@ class RuntimeValidator:
     def __init__(self):
         self._system_validator = SystemValidator()
     
-    def validate(self, runtime_1: Runtime, runtime_2: Runtime):
-        if not runtime_1:
+    def validate(self, runtimes_1: List[Runtime], runtimes_2: List[Runtime]):
+        if not runtimes_1:
             return
-        if not runtime_2:
-            raise(ValueError(Errors.ELEMENT_MISSING.format(runtime_1)))
-        if runtime_1.arch == runtime_2.arch:
-            if runtime_2.systems:
-                self._systems_validate(runtime_1.systems, runtime_2.systems[0])
-            return True
-        return False
-    
-    def _systems_validate(self, systems_1:List[System], system_2:System):
-        for system_1 in systems_1:
-            self._system_validator.validate(system_1, system_2)
+        if not runtimes_2:
+            raise(ValueError(Errors.ELEMENT_MISSING.format(runtimes_1)))
+        runtime_2 = runtimes_2[0]
+        for runtime_1 in runtimes_1:
+            if runtime_1.arch == runtime_2.arch:
+                if runtime_1.systems:
+                    self._system_validator.validate(runtime_1.systems, runtime_2.systems)
+                return
