30ec7f2ba95619b2bea176808356d9b817c4467e
Luca Atella <atellaluca@outlook.it>
2025-03-10 12:47:33 +0100

    fix: patch jinja version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'ImportSpy'
 copyright = '2024, Luca Atella'
 author = 'Luca Atella'
-release = '0.2.0'
+release = '0.2.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
