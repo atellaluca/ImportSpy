110197e7ce0516bc767ab332713e74802a91f772
Luca Atella <atellaluca@outlook.it>
2025-05-02 12:55:37 +0200

    chore: bump to version 0.3.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'ImportSpy'
 copyright = '2024, Luca Atella'
 author = 'Luca Atella'
-release = '0.3.2'
+release = '0.3.3'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
